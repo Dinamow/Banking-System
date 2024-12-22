569a18632fefa2a8e4a151f1dbbcd9a8ac19f3de
DINAMOW <meemoo102039@gmail.com>
2024-12-23 01:22:13 +0200

    init DB models

---
+++ Banking-System/Models/Account.cs
@@ -0,0 +1,14 @@
+﻿namespace Banking_System.Models
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public required string AccountNumber { get; set; }
+        public required string AccountType { get; set; }
+        public int Balance { get; set; }
+        public int OverDraftLimit { get; set; }
+        public float IntrestRate { get; set; }
+        public DateTime CreateAt { get; set; }
+        public DateTime UpdateAt { get; set; }
+    }
+}
---
+++ Banking-System/Models/Transaction.cs
@@ -0,0 +1,13 @@
+﻿namespace Banking_System.Models
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public int AccountId { get; set; }
+        public required string TransactionType { get; set; }
+        public int Amount { get; set; }
+        public int TargetAccount { get; set; }
+        public DateTime CreateAt { get; set; }
+        public DateTime UpdateAt { get; set; }
+    }
+}
