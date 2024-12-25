3b628b5384bbd3e672317a5c3191ab0ea81e9768
DINAMOW <meemoo102039@gmail.com>
2024-12-25 02:52:00 +0200

    feat(Auth): impel Register Endpoint

---
+++ Banking-System/Presentation/Controllers/AuthController.cs
@@ -0,0 +1,40 @@
+﻿using Banking_System.Application.Auth.DTOs;
+using Banking_System.Application.Auth.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+public class AuthController : ControllerBase
+{
+    private readonly IAuthService _authService;
+
+    public AuthController(IAuthService authService)
+    {
+        _authService = authService ?? throw new ArgumentNullException(nameof(authService));
+    }
+
+    /// <summary>
+    /// Register a new user.
+    /// </summary>
+    /// <remarks>
+    /// This endpoint allows users to register a new account.
+    /// </remarks>
+    /// <response code="200"> Returns a JSON object with a success message.</response>
+    [HttpPost("register")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(RegisterResponseDTO))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> Register(AuthRequestDTO request)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        var response = await _authService.RegisterAsync(request);
+        if (!response.Success)
+        {
+            return BadRequest(response);
+        }
+
+        return Ok(response);
+    }
+}
