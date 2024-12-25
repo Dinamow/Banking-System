9261ff17f1710f6a632b8bfe4c8a5819f9ab2cba
DINAMOW <meemoo102039@gmail.com>
2024-12-25 02:51:07 +0200

    feat(Auth): Auth behavior

---
+++ Banking-System/Application/Auth/Interfaces/IAuthService.cs
@@ -0,0 +1,12 @@
+﻿using Banking_System.Application.Auth.DTOs;
+
+namespace Banking_System.Application.Auth.Interfaces
+{
+    public interface IAuthService
+    {
+        // for login
+        Task<AuthResponseDTO> AuthenticateAsync(AuthRequestDTO request);
+        // for registration
+        Task<RegisterResponseDTO> RegisterAsync(AuthRequestDTO request);
+    }
+}
