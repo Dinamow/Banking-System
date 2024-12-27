6f73bb8d7ba93065f217f95a1279ba5abff7cc35
DINAMOW <meemoo102039@gmail.com>
2024-12-27 04:12:48 +0200

    fead(BankingAccount): Add Account DTO

---
+++ Banking-System/Application/Account/DTOs/AccountDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace Banking_System.Application.Account.DTOs
+{
+    public class AccountDTO
+    {
+        public int Id { get; set; }
+        public required string AccountNumber { get; set; }
+        public required string AccountType { get; set; }
+        public int Balance { get; set; }
+        public int OverDraftLimit { get; set; }
+        public float IntrestRate { get; set; }
+        public decimal AccumulatedInterest { get; set; }
+        public DateTime LastInterestCalculationDate { get; set; }
+        public DateTime CreateAt { get; set; }
+        public DateTime UpdateAt { get; set; }
+        public required string UserId { get; set; }
+    }
+}
