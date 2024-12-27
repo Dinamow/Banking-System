c8188bd2d7a0d03383cdbd5838dcc24273c4f516
DINAMOW <meemoo102039@gmail.com>
2024-12-27 04:13:42 +0200

    feat(BankingAcccocunt): add mapping

---
+++ Banking-System/Application/Account/Mapping/AccountMapping.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Banking_System.Application.Account.DTOs;
+using Banking_System.Core.Models;
+
+namespace Banking_System.Application.Account.Mapping
+{
+    public class AccountMapping : Profile
+    {
+        public AccountMapping()
+        {
+            CreateMap<BankingAccount, AccountDTO>();
+        }
+    }
+}
--- Banking-System/Presentation/Configurations/AutoMapperConfig.cs
+++ Banking-System/Presentation/Configurations/AutoMapperConfig.cs
@@ -1,11 +1,12 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using AutoMapper;
+﻿using AutoMapper;
 using Banking_System.Application.Auth.Mapping;
+using Banking_System.Application.Account.Mapping;
 
 public static class AutoMapperConfig
 {
     public static void AddAutoMapperProfiles(this IServiceCollection services)
     {
         services.AddAutoMapper(typeof(AuthMapping).Assembly);
+        services.AddAutoMapper(typeof(AccountMapping).Assembly);
     }
 }
