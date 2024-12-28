9bb3bcb0e1007550f585c4a144ad3439535bdc31
DINAMOW <meemoo102039@gmail.com>
2024-12-28 23:56:17 +0200

    Fix: use Decimal instead of float and int

---
+++ Banking-System/Migrations/20241228162845_ChangeIntToDecimal.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Banking_System.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeIntToDecimal : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Amount",
+                table: "Transactions",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "OverDraftLimit",
+                table: "Accounts",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldDefaultValue: 0);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Balance",
+                table: "Accounts",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldDefaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Amount",
+                table: "Transactions",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "OverDraftLimit",
+                table: "Accounts",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldDefaultValue: 0m);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Balance",
+                table: "Accounts",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldDefaultValue: 0m);
+        }
+    }
+}
