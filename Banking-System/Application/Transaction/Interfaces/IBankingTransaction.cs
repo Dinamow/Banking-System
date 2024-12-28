4b18031e63d137a02a1fd73affe17adfce1a0b36
DINAMOW <meemoo102039@gmail.com>
2024-12-29 00:11:09 +0200

    Feat(Transaction): Impelement Transaction Interface

---
+++ Banking-System/Application/Transaction/Interfaces/IBankingTransaction.cs
@@ -0,0 +1,9 @@
+﻿using Banking_System.Core.Models;
+
+namespace Banking_System.Application.Transaction.Interfaces
+{
+    public interface IBankingTransaction
+    {
+        Task<BankingTransaction> CreateTransactionAsync(string UserId, int AccountId, string TransactionType, decimal Amount, int TargetAccount);
+    }
+}
