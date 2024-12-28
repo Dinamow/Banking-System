b6aedd0ea57769fbf2c7703d4d56f737f70d32e6
DINAMOW <meemoo102039@gmail.com>
2024-12-29 00:13:21 +0200

    feat(Transaction): Impelement Transaction mapping

---
+++ Banking-System/Application/Transaction/Mapping/TransactionMapping.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Banking_System.Application.Transaction.DTOs;
+using Banking_System.Core.Models;
+
+namespace Banking_System.Application.Transaction.Mapping
+{
+    public class TransactionMapping : Profile
+    {
+        public TransactionMapping()
+        {
+            CreateMap<BankingTransaction, TransactionDTO>();
+        }
+    }
+}
