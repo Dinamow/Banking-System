721896a21d026130047996f5e5047da874e28721
DINAMOW <meemoo102039@gmail.com>
2024-12-28 13:01:45 +0200

    chor(DBcontext): Update Database context

---
+++ Banking-System/Migrations/20241227214615_MakeAccountNumberAuto.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Banking_System.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeAccountNumberAuto : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "AccountNumber",
+                table: "Accounts",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValueSql: "NEWID()",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "AccountNumber",
+                table: "Accounts",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20,
+                oldDefaultValueSql: "NEWID()");
+        }
+    }
+}
