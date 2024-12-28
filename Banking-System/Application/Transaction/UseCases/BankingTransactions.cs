4746ff6fa865a97f955da24c529c81e88e11e7a4
DINAMOW <meemoo102039@gmail.com>
2024-12-29 00:25:01 +0200

    feat(Transaction): Impelement Transaction Endpoints

---
+++ Banking-System/Application/Transaction/UseCases/BankingTransactions.cs
@@ -0,0 +1,116 @@
+﻿using Banking_System.Application.Account.Interfaces;
+using Banking_System.Application.Transaction.Interfaces;
+using Banking_System.Core.Models;
+using Banking_System.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+public class BankingTransactions : IBankingTransaction
+{
+    private readonly BankingDataContext _context;
+    private readonly IAccountService _accountService;
+    private const decimal OVER_DRAFT_LIMIT = 500;
+
+    public BankingTransactions(BankingDataContext context, IAccountService accountService)
+    {
+        _context = context ?? throw new ArgumentNullException(nameof(context));
+        _accountService = accountService ?? throw new ArgumentNullException(nameof(accountService));
+    }
+
+    public async Task<BankingTransaction> CreateTransactionAsync(string UserId, int AccountId, string transactionType, decimal Amount, int TargetAccount)
+    {
+        if (Amount <= 0)
+        {
+            throw new ArgumentException("Transaction amount must be greater than zero.");
+        }
+
+        var SourceAccount = await _accountService.GetAccountAsync(AccountId);
+        var ToAccount = TargetAccount != 0 ? await _accountService.GetAccountAsync(TargetAccount) : SourceAccount;
+
+        if (SourceAccount == null) throw new ArgumentException("Source account not found.");
+        if (transactionType == "transfer" && ToAccount == null) throw new ArgumentException("Target account not found.");
+
+        switch (transactionType)
+        {
+            case "deposit":
+                HandleDeposit(SourceAccount, Amount);
+                break;
+            case "withdraw":
+                await _accountService.GetMyAccountAsync(UserId, AccountId);
+                if (!CanWithdraw(SourceAccount, Amount)) throw new InvalidOperationException("Insufficient funds.");
+                HandleWithdraw(SourceAccount, Amount);
+                break;
+            case "transfer":
+                await _accountService.GetMyAccountAsync(UserId, AccountId);
+                if (!CanWithdraw(SourceAccount, Amount)) throw new InvalidOperationException("Insufficient funds.");
+                HandleWithdraw(SourceAccount, Amount);
+                HandleDeposit(ToAccount, Amount);
+                break;
+            default:
+                throw new InvalidOperationException("Invalid Transaction Type");
+        }
+        _context.Update(SourceAccount);
+        if (ToAccount != null && ToAccount != SourceAccount) _context.Update(ToAccount);
+        return await CreateTransactionRecord(AccountId, transactionType, Amount, TargetAccount);
+    }
+
+    private bool CanWithdraw(BankingAccount account, decimal amount)
+    {
+        decimal overdraftLimit = account.AccountType == "SavingsAccount" ? 0 : OVER_DRAFT_LIMIT - account.OverDraftLimit;
+        return account.Balance + overdraftLimit >= amount;
+    }
+
+    private async Task<BankingTransaction> CreateTransactionRecord(int AccountId, string transactionType, decimal Amount, int TargetAccount)
+    {
+        var transaction = new BankingTransaction
+        {
+            AccountId = AccountId,
+            TransactionType = transactionType.ToString(),
+            Amount = Amount,
+            TargetAccount = TargetAccount != 0 ? TargetAccount : AccountId
+        };
+        _context.Transactions.Add(transaction);
+        await _context.SaveChangesAsync();
+        return transaction;
+    }
+    private void HandleWithdraw(BankingAccount account, decimal amount)
+    {
+        if (account.AccountType == "SavingsAccount")
+        {
+            account.Balance -= amount;
+        }
+        else
+        {
+            decimal overdraftLimit = OVER_DRAFT_LIMIT - account.OverDraftLimit;
+            if (account.Balance >= amount)
+            {
+                account.Balance -= amount;
+            }
+            else if (account.Balance + overdraftLimit >= amount)
+            {
+                account.OverDraftLimit += amount - account.Balance;
+                account.Balance = 0;
+            }
+            else
+            {
+                throw new InvalidOperationException("Insufficient funds.");
+            }
+        }
+    }
+    private void HandleDeposit(BankingAccount account, decimal amount)
+    {
+        if (account.OverDraftLimit > 0)
+        {
+            if (amount >= account.OverDraftLimit)
+            {
+                amount -= account.OverDraftLimit;
+                account.OverDraftLimit = 0;
+            }
+            else
+            {
+                account.OverDraftLimit -= amount;
+                amount = 0;
+            }
+        }
+        account.Balance += amount;
+    }
+}
---
+++ Banking-System/Presentation/Controllers/TransactionController.cs
@@ -0,0 +1,106 @@
+﻿using AutoMapper;
+using Banking_System.Application.Transaction.DTOs;
+using Banking_System.Application.Transaction.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Banking_System.Presentation.Controllers
+{
+    [Route("api/account/")]
+    [ApiController]
+    [Authorize]
+    public class TransactionController : ControllerBase
+    {
+        private readonly IBankingTransaction _transaction;
+        private readonly IMapper _mapper;
+        public TransactionController(IBankingTransaction transaction, IMapper mapper)
+        {
+            _transaction = transaction;
+            _mapper = mapper;
+        }
+        /// <summary>
+        /// Deposit money into an account.
+        /// </summary>
+        /// <param name="depositDTO">
+        /// The deposit data transfer object.
+        /// </param>
+        /// <returns>
+        /// A JSON object with the transaction details.
+        /// </returns>
+        [HttpPost]
+        [Route("deposit")]
+        [ProducesResponseType(200, Type = typeof(TransactionDTO))]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        public async Task<IActionResult> Deposit([FromForm] DepositDTO depositDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                var transaction = await _transaction.CreateTransactionAsync(string.Empty, depositDTO.Id, "deposit", depositDTO.Amount, 0);
+                var transactionDTO = _mapper.Map<TransactionDTO>(transaction);
+                return Ok(transactionDTO);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        /// <summary>
+        /// Withdraw money from an account.
+        /// </summary>
+        /// <param name="withdrawDTO">
+        /// The deposit data transfer object.
+        /// </param>
+        /// <returns>
+        /// A JSON object with the transaction details.
+        /// </returns>
+        [HttpPost]
+        [Route("withdraw")]
+        [ProducesResponseType(200, Type = typeof(TransactionDTO))]
+        [ProducesResponseType(400)]
+        public async Task<IActionResult> Withdraw([FromForm] DepositDTO withdrawDTO)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+            try
+            {
+                var UserId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+                var transaction = await _transaction.CreateTransactionAsync(UserId, withdrawDTO.Id, "withdraw", withdrawDTO.Amount, 0);
+                var transactionDTO = _mapper.Map<TransactionDTO>(transaction);
+                return Ok(transactionDTO);
+            }
+            catch (Exception ex) { return BadRequest(ex.Message); }
+        }
+        /// <summary>
+        /// Transfer money from acccount to acccount
+        /// </summary>
+        /// <param name="transDTO">
+        /// The transfer data transfer object.
+        /// </param>
+        /// <returns>
+        /// A JSON object with the transaction details.
+        /// </returns>
+        [HttpPost]
+        [Route("transfer")]
+        [ProducesResponseType(200, Type = typeof(TransactionDTO))]
+        [ProducesResponseType(400)]
+        public async Task<IActionResult> Transfer([FromForm] TransferDTO transDTO)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+            try
+            {
+                var UserId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+                var transaction = await _transaction.CreateTransactionAsync(UserId, transDTO.FromAccount, "transfer", transDTO.Amount, transDTO.ToAccount);
+                var transactionDTO = _mapper.Map<TransactionDTO>(transaction);
+                return Ok(transactionDTO);
+            }
+            catch (Exception ex) { return BadRequest(ex.Message); }
+        }
+    }
+}
